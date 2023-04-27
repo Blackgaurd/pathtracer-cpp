e473e5ad5f7d6178e31d223335967ac0f33ce11c
bryan <bryan.deng002@gmail.com>
2023-04-27 01:29:27 -0400

    revert to stable version

--- pathtracer/scene.h
+++ pathtracer/scene.h
@@ -16,7 +16,7 @@
 #include "tiny_obj_loader.h"
 
 struct Scene {
-    std::vector<ObjectPtr> objects, bvh_objects;
+    std::vector<ObjectPtr> objects;
     BVHNodePtr bvh_root;
 
     float shift_bias = 1e-4;
@@ -24,13 +24,7 @@ struct Scene {
     Scene() = default;
 
     template <typename T, typename... Args>
-    void bvh_add_object(Args&&... args) {
-        // add an object to be accelerated by the BVH
-        bvh_objects.push_back(std::make_shared<T>(std::forward<Args>(args)...));
-    }
-    template <typename T, typename... Args>
     void add_object(Args&&... args) {
-        // add an object to the scene (not accelerated by the BVH)
         objects.push_back(std::make_shared<T>(std::forward<Args>(args)...));
     }
     void load_obj(const std::string& filename, const std::string& mtl_path = "./") {
@@ -89,7 +83,7 @@ struct Scene {
                     }
                 }
 
-                bvh_add_object<Triangle>(points[0], points[1], points[2], material);
+                add_object<Triangle>(points[0], points[1], points[2], material);
             }
         }
     }
@@ -98,7 +92,7 @@ struct Scene {
         if (objects.empty()) throw std::runtime_error("no objects in scene");
 
         std::cout << "Building BVH..." << std::endl;
-        bvh_root = build_bvh(bvh_objects);
+        bvh_root = build_bvh(objects);
         std::cout << "Done" << std::endl;
         // return;
 
@@ -112,11 +106,16 @@ struct Scene {
                 vec3 color = {0, 0, 0};
                 for (int s = 0; s < samples; s++) {
                     camera.get_ray(w, h, ray_o, ray_d);
+#ifdef ITERATIVE
+                    color += path_trace_iter(ray_o, ray_d, depth);
+#else
                     color += path_trace(ray_o, ray_d, depth);
+#endif
                 }
                 image.set_pixel(w, h, color / samples);
             }
         }
+        std::cout << "\rRendering: 100%   " << std::endl;
     }
     void run_thread(const Camera& camera, Image& image, int depth, int samples) {
         Resolution res = image.res;
@@ -126,7 +125,11 @@ struct Scene {
                 vec3 color = {0, 0, 0};
                 for (int s = 0; s < samples; s++) {
                     camera.get_ray(w, h, ray_o, ray_d);
+#ifdef ITERATIVE
+                    color += path_trace_iter(ray_o, ray_d, depth);
+#else
                     color += path_trace(ray_o, ray_d, depth);
+#endif
                 }
                 vec3 prev_color = image.get_pixel(w, h);
                 image.set_pixel(w, h, prev_color + color);
@@ -144,7 +147,7 @@ struct Scene {
         }
 
         std::cout << "Building BVH..." << std::endl;
-        bvh_root = build_bvh(bvh_objects);
+        bvh_root = build_bvh(objects);
         std::cout << "Done" << std::endl;
 
         std::vector<std::thread> pool(threads);
@@ -159,47 +162,12 @@ struct Scene {
         for (const Image& img : images) image += img;
         image /= samples;
     }
-    ObjectPtr intersect(const vec3& ray_o, const vec3& ray_d, float& hit_t,
-                        const ObjectPtr& ignore = nullptr) {
-        float bvh_t;
-        ObjectPtr bvh_hit_obj = intersect_bvh(bvh_root, ray_o, ray_d, hit_t, ignore);
-
-        float bf_t;  // brute force
-        ObjectPtr hit_obj = nullptr;
-        hit_t = 1e30;
-        for (ObjectPtr& obj : objects) {
-            if (obj == ignore) continue;
-
-            float t;
-            if (obj->intersect(ray_o, ray_d, t) && t < bf_t) {
-                bf_t = t;
-                hit_obj = obj;
-            }
-        }
-
-        if (bvh_hit_obj && hit_obj) {
-            if (bvh_t < bf_t) {
-                hit_t = bvh_t;
-                return bvh_hit_obj;
-            } else {
-                hit_t = bf_t;
-                return hit_obj;
-            }
-        } else if (bvh_hit_obj) {
-            hit_t = bvh_t;
-            return bvh_hit_obj;
-        } else if (hit_obj) {
-            hit_t = bf_t;
-            return hit_obj;
-        }
-        return nullptr;
-    }
     vec3 path_trace(const vec3& ray_o, const vec3& ray_d, int depth) {
         // recursive path tracing
         if (depth == 0) return {0, 0, 0};
 
         float hit_t;
-        ObjectPtr hit_obj = intersect(ray_o, ray_d, hit_t);
+        ObjectPtr hit_obj = intersect_bvh(bvh_root, ray_o, ray_d, hit_t);
         if (!hit_obj) return {0, 0, 0};
 
         if (hit_obj->material->type == EMIT) {
